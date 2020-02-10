eaeaa6b3676759800b143132818a92d714b85503
Bui Quang Minh <minh.buiquang@arsvietnam.com>
2020-02-10 14:07:22 +0700

    add vgg_checkpoint parameter, change return type of foward_fn, calc loss

--- nets/craft_at_synthtext.py
+++ nets/craft_at_synthtext.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 from nets.abstract_model_helper import AbstractModelHelper
 from utils.external.craft_tensorflow.model import craft as CRAFT
-from utils.external.craft_tensorflow.loss import MSE_OHEM_Loss
-from utils.external.craft_tensorflow.utils import decay_learning_rate, calculate_fscore
+from utils.external.craft_tensorflow.loss import MSE_OHEM_Loss, calculate_fscore
+from utils.external.craft_tensorflow.utils import decay_learning_rate
 from datasets.synthtext_dataset import SynthTextDataset
+import tensornets as nets
+
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_integer('epoch', 20, ' ')
-# tf.app.flags.DEFINE_integer('batch_size', 20, ' ')
+tf.app.flags.DEFINE_integer('epoch', 1, ' ')
+tf.app.flags.DEFINE_string('vgg_ckpt', '/hdd/Minhbq/PocketFlow_remove/backbone_models/vgg_16/vgg_16.ckpt', 'VGG tensorflow CKPT path')
 tf.app.flags.DEFINE_float('learning_rate', 0.001, ' ')
 tf.app.flags.DEFINE_float('decay_factor_lr', 0.0125/4000, ' ')
 tf.app.flags.DEFINE_float('momentum', 0.9, 'momentum coefficient')
@@ -24,7 +26,8 @@ def forward_fn(inputs, is_train, data_format):
     * outputs from the network's forward pass
     """
     VGG16, y_pred = CRAFT(inputs, is_train)
-    return y_pred
+    output = {'heatmaps': y_pred, 'is_train': tf.constant(is_train)}
+    return output
 
 class ModelHelper(AbstractModelHelper):
     """Model helper for creating a CRAFT model for the SynthText dataset."""
@@ -62,23 +65,33 @@ class ModelHelper(AbstractModelHelper):
     def calc_loss(self, labels, outputs, trainable_vars):
         """Calculate loss (and some extra evaluation metrics).
         Args:
-        * inputs: inputs to the network's forward pass
-        * is_train: whether to use the forward pass with training operations inserted
-        * data_format: data format ('channels_last' OR 'channels_first')
+        * labels: dict
+        * outputs: heatmaps
+        # * data_format: data format ('channels_last' OR 'channels_first')
 
         Returns:
         * loss: loss calculated
         * metrics: dict (metrics that calculated (accuracy,...))
         """
-        
+        heatmaps = outputs['heatmaps']
+        is_train = outputs['is_train']
         weight_characters = labels['weight_characters']
         weight_affinitys = labels['weight_affinitys']
-
+        # char_coords = labels['char_coords']
+        
         confident_maps = tf.ones_like(weight_characters)
         label = tf.stack((weight_characters, weight_affinitys), axis=-1)
-        loss = MSE_OHEM_Loss(outputs, label, confident_maps)
-        # metrics = {'fscore': calculate_fscore(outputs, y)}
-        metrics = {'acc' : tf.constant(0.9)}
+
+        def true_fn():
+            return MSE_OHEM_Loss(heatmaps, label, confident_maps, True)
+
+        def false_fn():
+            return MSE_OHEM_Loss(heatmaps, label, confident_maps, False)
+            
+        loss = tf.cond(is_train, true_fn, false_fn)
+        metrics = {}
+        # fscore = tf.py_function(calculate_fscore, [weight_characters, weight_affinitys, char_coords], (tf.float32))
+        # metrics = {'fscore': fscore}
         return loss, metrics
 
     def setup_lrn_rate(self, global_step):
