3c50a2327c798617e3d0fa968b971e103f3a3244
Nam Doan Ngoc <giang.doanngoc@arsvietnam.com>
2020-02-11 09:19:57 +0700

    _bytes_feature image and weight character/affininty

--- utils/external/craft_tensorflow/synthtext_to_tfrecord.py
+++ utils/external/craft_tensorflow/synthtext_to_tfrecord.py
@@ -10,12 +10,15 @@ import base64
 from augment import rand_augment
 from data_manipulation import resize, generate_target, generate_affinity, normalize_mean_variance
 import multiprocessing
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+import threading
+from datetime import datetime
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
-DATA_MODE = 'val'
+DATA_MODE = 'train'
 SYNTH_IMG_PATH = '/hdd/UBD/background-images/data_generated/Pocketflow/{}/synth'.format(DATA_MODE)
-NUMBER_OF_THREAD = 4
+NUMBER_OF_THREAD = 1
 AUGUMENT = True if DATA_MODE == 'train' else False
+print('AUGMENT: ', AUGUMENT)
 
 def _int64_feature(value):
     """Wrapper for inserting int64 features into Example proto."""
@@ -51,8 +54,76 @@ def scale(img, newrange=(0,255), eps = 1e-8):
     img = (newrange[1] - newrange[0])*(img - min_)/(max_ - min_ + eps) + newrange[0]
     return img
 
+class ImageCoder(object):
+    """Helper class that provides TensorFlow image coding utilities."""
+
+    def __init__(self):
+        # Create a single Session to run all image coding calls.
+        self._sess = tf.Session()
+
+        # Initializes function that converts PNG to JPEG data.
+        self._png_data = tf.placeholder(dtype=tf.string)
+        image = tf.image.decode_png(self._png_data, channels=3)
+        self._png_to_jpeg = tf.image.encode_jpeg(image, format='rgb', quality=100)
+
+        # Initializes function that converts CMYK JPEG data to RGB JPEG data.
+        self._cmyk_data = tf.placeholder(dtype=tf.string)
+        image = tf.image.decode_jpeg(self._cmyk_data, channels=0)
+        self._cmyk_to_rgb = tf.image.encode_jpeg(image, format='rgb', quality=100)
+
+        # Initializes function that decodes RGB JPEG data.
+        self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
+        self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
+
+    def png_to_jpeg(self, image_data):
+        return self._sess.run(self._png_to_jpeg,
+                            feed_dict={self._png_data: image_data})
+
+    def cmyk_to_rgb(self, image_data):
+        return self._sess.run(self._cmyk_to_rgb,
+                            feed_dict={self._cmyk_data: image_data})
+
+    def decode_jpeg(self, image_data):
+        image = self._sess.run(self._decode_jpeg,
+                            feed_dict={self._decode_jpeg_data: image_data})
+        assert len(image.shape) == 3
+        assert image.shape[2] == 3
+        return image
+
+def _process_image(filename, coder):
+    """Process a single image file.
+    Args:
+        filename: string, path to an image file e.g., '/path/to/example.JPG'.
+        coder: instance of ImageCoder to provide TensorFlow image coding utils.
+    Returns:
+        image_buffer: string, JPEG encoding of RGB image.
+        height: integer, image height in pixels.
+        width: integer, image width in pixels.
+    """
+    # Read the image file.
+    image_data = tf.gfile.GFile(filename, 'rb').read()
+
+    print('Converting PNG to JPEG for %s' % filename)
+    image_data = coder.png_to_jpeg(image_data)
+
+    print('Encoded')
+
+    # Decode the RGB JPEG.
+    image = coder.decode_jpeg(image_data)
+    print('Decoded')
+
+    # Check that image converted to RGB
+    assert len(image.shape) == 3
+    height = image.shape[0]
+    width = image.shape[1]
+    channels = image.shape[2]
+    assert channels == 3
+
+    return image_data, image, height, width, channels
+
 def convert_to_example(
-    filename, image_name, image_buffer, char_bbs, word_texts, height, width, channels):
+    filename, image_name, image_buffer, weight_character_buffer, weight_affinity_buffer, char_bbs, word_texts, height, width, channels):
+    # print(image_buffer[0].shape, image_buffer[1].shape, image_buffer[2].shape)
     """Build an Example proto for an example.
 
         Args:
@@ -90,10 +161,10 @@ def convert_to_example(
     example = tf.train.Example(
         features=tf.train.Features(
             feature={
-                "image/height": _int64_feature(height),
-                "image/width": _int64_feature(width),
-                "image/channels": _int64_feature(channels),
-                "image/shape": _int64_feature([height, width, channels]),
+                # "image/height": _int64_feature(height),
+                # "image/width": _int64_feature(width),
+                # "image/channels": _int64_feature(channels),
+                # "image/shape": _int64_feature([height, width, channels]),
                 "image/object/x_top_left": _float_feature(x_top_left),
                 "image/object/y_top_left": _float_feature(y_top_left),
                 "image/object/x_top_right": _float_feature(x_top_right),
@@ -102,27 +173,26 @@ def convert_to_example(
                 "image/object/y_bottom_right": _float_feature(y_bottom_right),
                 "image/object/x_bottom_left": _float_feature(x_bottom_left),
                 "image/object/y_bottom_left": _float_feature(y_bottom_left),
-                "image/object/words": _bytes_list_feature(word_texts),
+                # "image/object/words": _bytes_list_feature(word_texts),
                 "image/format": _bytes_feature(image_format),
                 "image/filename": _bytes_feature(image_name.encode("utf8")),
-                "image/image/encoded": _float_feature(image_buffer[0]),
-                "image/weight_character/encoded": _float_feature(image_buffer[1]),
-                "image/weight_affinity/encoded": _float_feature(image_buffer[2]),
+                "image/image/encoded": _bytes_feature(image_buffer),
+                "image/weight_character/encoded": _bytes_feature(weight_character_buffer),
+                "image/weight_affinity/encoded": _bytes_feature(weight_affinity_buffer),
             }
         )
     )
     return example
 
 def encode_jpeg(data):
-    format = 'rgb'
-    if len(data.shape) == 2:
-        format = 'grayscale'
+    format_type = 'rgb'
+    if len(data.shape) <= 2:
         data = data[..., None]
-    print(data)
+        format_type = 'grayscale'
     g = tf.Graph()
     with g.as_default():
         data_t = tf.placeholder(tf.uint8)
-        op = tf.image.encode_jpeg(data_t, format=format, quality=100)
+        op = tf.image.encode_jpeg(data_t, format=format_type, quality=100)
         init = tf.initialize_all_variables()
 
     with tf.Session(graph=g) as sess:
@@ -130,14 +200,15 @@ def encode_jpeg(data):
         data_np = sess.run(op, feed_dict={ data_t: data })
     return data_np
 
-def process_and_write_tfrecord(current_process, imnames, charBBs, txts, writer):
+def process_and_write_tfrecord(current_process, imnames, charBBs, txts, writer, coder):
     for i, imname in enumerate(imnames):
         filename = imname[0]
         print('Process {} === {}/{}: Processing {} \n'.format(current_process, i, len(imnames), filename), end='\r')
         img_path = os.path.join(SYNTH_IMG_PATH, 'img', filename)
-        # Read RGB
-        image = cv2.imread(img_path)[:,:,::-1]     # change from BGR to RGB image
-        height, width, channels = image.shape
+
+        # Read to image buffer
+        image_buffer, image, height, width, channels = _process_image(img_path, coder)
+        image = image[:,:,::-1]
         
         # character bounding box
         char_bbs = charBBs[i] #shape: (2, 4, ?)
@@ -152,6 +223,14 @@ def process_and_write_tfrecord(current_process, imnames, charBBs, txts, writer):
         chw_image = (image.copy()).transpose(2, 0, 1)
         weight_character = generate_target(chw_image.shape, character.copy())
         weight_affinity, affinity_bbox = generate_affinity(chw_image.shape, character.copy(), word_texts.copy())
+        weight_character_buffer = np.clip((weight_character * 255).astype(np.uint8), 0, 255)
+        weight_affinity_buffer = np.clip((weight_affinity * 255).astype(np.uint8), 0, 255)
+
+        # print(weight_character_buffer, np.min(weight_character_buffer), np.max(weight_character_buffer))
+        # print(weight_affinity_buffer, np.min(weight_affinity_buffer), np.max(weight_affinity_buffer))
+        weight_character_buffer = encode_jpeg(weight_character_buffer)
+        weight_affinity_buffer = encode_jpeg(weight_affinity_buffer)
+
         if AUGUMENT:
             imgs = rand_augment(list([image.copy(), weight_character.copy(), weight_affinity.copy()]))
         else:
@@ -161,7 +240,7 @@ def process_and_write_tfrecord(current_process, imnames, charBBs, txts, writer):
         # print(np.unique(imgs[1]))
         # print(np.max(imgs[2]))
         image_data = [img.flatten().tolist() for img in imgs]
-        example = convert_to_example(img_path, filename, image_data, char_bbs, word_texts,
+        example = convert_to_example(img_path, filename, image_buffer, weight_character_buffer, weight_affinity_buffer, char_bbs, word_texts,
                                 height, width, channels)
         # print(image_data[0].shape)
         # print(image_data[1].shape)
@@ -172,6 +251,12 @@ def process_and_write_tfrecord(current_process, imnames, charBBs, txts, writer):
     writer.close()
 
 if __name__ == "__main__":
+    # Create a generic TensorFlow-based utility for converting all image codings.
+    coder = ImageCoder()
+
+    # Create a mechanism for monitoring when all threads are finished.
+    coord = tf.train.Coordinator()
+
     mat = loadmat(os.path.join(SYNTH_IMG_PATH, 'bg.mat'))
     imnames = mat['imnames'][0] # ['file1.png', 'file2.png', ..., 'finen.png']
     charBBs = mat['charBB'][0]  # number of images, 2, 4, num_character
@@ -180,17 +265,32 @@ if __name__ == "__main__":
     # multiprocessing
     indexes = np.arange(len(imnames))
     chunks = np.array_split(indexes, NUMBER_OF_THREAD)
-    processes = []
-    for index in range(NUMBER_OF_THREAD):
-        writer = tf.python_io.TFRecordWriter('/hdd/namdng/tf_record_dataset/craft/{}_{}.tfrecord'.format(DATA_MODE, index))
-        p = multiprocessing.Process(target=process_and_write_tfrecord, args=(index, imnames[chunks[index]], charBBs[chunks[index]], txts[chunks[index]], writer))
-        processes.append(p)
 
-    for p in processes:
-        p.start()
+    # writer = tf.python_io.TFRecordWriter('/hdd/namdng/tf_record_dataset/synth_text/{}_{}.tfrecord'.format(DATA_MODE, 0))
+    # process_and_write_tfrecord(0, imnames[chunks[0]], charBBs[chunks[0]], txts[chunks[0]], writer, coder)
+    # processes = []
+    # for index in range(NUMBER_OF_THREAD):
+    #     writer = tf.python_io.TFRecordWriter('/hdd/namdng/tf_record_dataset/synth_text/{}_{}.tfrecord'.format(DATA_MODE, index))
+    #     p = multiprocessing.Process(target=process_and_write_tfrecord, args=(index, imnames[chunks[index]], charBBs[chunks[index]], txts[chunks[index]], writer, coder))
+    #     processes.append(p)
 
-    for p in processes:
-        p.join()
+    # for p in processes:
+    #     p.start()
+
+    # for p in processes:
+    #     p.join()
     
+    threads = []
+    for index in range(NUMBER_OF_THREAD):
+        writer = tf.python_io.TFRecordWriter('/hdd/namdng/tf_record_dataset/synth_text/{}_{}.tfrecord'.format(DATA_MODE, index))
+        t = threading.Thread(target=process_and_write_tfrecord, args=(index, imnames[chunks[index]], charBBs[chunks[index]], txts[chunks[index]], writer, coder))
+        t.start()
+        threads.append(t)
+
+    # Wait for all the threads to terminate.
+    coord.join(threads)
+    print('%s: Finished writing all %d images in data set.' %
+            (datetime.now(), len(imnames)))
+    sys.stdout.flush()
 
     
